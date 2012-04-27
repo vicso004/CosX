4da5662d02027111b1b80c7af736bca4880eca6f
Christopher Visser <chrvi915@student.liu.se>
2012-04-27 10:23:28 +0200

    bootstrapade temat på matches

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,4 +2,6 @@ class Match < ActiveRecord::Base
   belongs_to :season
   has_and_belongs_to_many :players ##many players participate in matches
   has_many :goals #many goals in a match
+  
+  validates_presence_of :other_team, :message => "Fyll i match"
 end
