d4f1ec8e6f3a2d47daac5139d6ab4832e97e6439
Christopher Visser <chrvi915@student.liu.se>
2012-05-14 10:04:52 +0200

    connected goals matches and player with goalers and assists

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,6 +4,19 @@ class Player < ActiveRecord::Base
   has_many :matches, :through => :match_assignments#, :source => :match_id
   has_many :match_assignments
 	validates :name, :presence => true
-    
+  
+  has_many :Goals, :class_name => "Goal", :foreign_key => "goaler_id"
+  has_many :Goals, :class_name => "Goal", :foreign_key => "assister_id"
+  
 #  MatchAssignment
+
+
+def made_goals
+
+  Goal.where(:goaler_id => self.id).length
+end
+
+def made_assists
+  Goal.where(:assister_id => self.id).length
+end
 end
