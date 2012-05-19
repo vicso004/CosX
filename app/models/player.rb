ab9d4339dae9b81168fdf406e1b468108d1eb314
Victor Söderberg <vicso004@student.liu.se>
2012-05-19 19:36:18 +0200

    alla problem åtgärdade (se lista) utom match-formuläret som inte är alignat ordentligt

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,10 +3,10 @@ class Player < ActiveRecord::Base
   has_and_belongs_to_many :matches ## a player participates in many matches
   has_many :matches, :through => :match_assignments#, :source => :match_id
   has_many :match_assignments
-	validates :name, :presence => true
-  
   has_many :Goals, :class_name => "Goal", :foreign_key => "goaler_id"
-  has_many :Goals, :class_name => "Goal", :foreign_key => "assister_id"
+  has_many :Goals, :class_name => "Goal", :foreign_key => "assister_id"              
+  
+  validates_presence_of :name, :message => "Fyll i namn"
   
 #  MatchAssignment
 
