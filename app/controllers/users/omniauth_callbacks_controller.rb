2aa70bb2371f51172ea1b15c8f32ea1641e3c7a0
Christopher Visser <chrvi915@student.liu.se>
2012-05-21 11:00:12 +0200

    la till lite kommentarer

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,22 +2,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
   skip_authorization_check
   def facebook
-    # You need to implement the method below in your model
     
     auth = request.env["omniauth.auth"]
-    #render :text => request.env["omniauth.auth"].to_yaml
+    #render :text => request.env["omniauth.auth"].to_yaml för att se vad man får ut
     @user = User.find_for_facebook_oauth(auth, current_user)
 
     if @user.persisted?
       flash[:notice] = "Inloggad med Facebook"
 
       # Fixa allt det andra
-      @user.avatar_url = auth.info.image
+      @user.avatar_url = auth.info.image #Bild ska uppdateras
       if @user.player == nil
         @user.set_player(auth.info.name)
       end
-      sign_in_and_redirect @user, :event => :authentication
-    else
+      sign_in_and_redirect @user, :notice => "Inloggad med facebook"
+    else #Rädda om nått knas
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
